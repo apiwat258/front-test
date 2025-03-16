33d5374e090590e056d653993dfff4605207e57c
apiwat258 <apiwat02042546@gmail.com>
2025-03-16 20:20:20 +0700

    skdjdsads

--- app/Factory/FactoryDetails/page.tsx
+++ app/Factory/FactoryDetails/page.tsx
@@ -2,11 +2,14 @@
 export const dynamic = 'force-dynamic';
 
 import FactoryDetails from './FactoryDetails';
+import { Suspense } from 'react';
 
 const Page = () => {
     return (
         <div>
-            <FactoryDetails />
+            <Suspense fallback={<div>Loading...</div>}>
+                <FactoryDetails />
+            </Suspense>
         </div>
     );
 }
